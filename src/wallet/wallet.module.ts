d26f906b6461c4e93fa0dca267a7c31b13a7d6ee
xaxxoo <bnahmad83@gmail.com>
2026-03-16 15:49:59 +0100

    feat: add TransactionsModule to WalletModule imports

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,16 +1,17 @@
 // src/wallet/wallet.module.ts
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BlockchainModule } from '../blockchain/blockchain.module';
-import { RatesModule } from '../rates/rates.module';
-import { User } from '../auth/entities/user.entity';
-import { WalletController } from './wallet.controller';
-import { WalletService } from './wallet.service';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { BlockchainModule } from '../blockchain/blockchain.module'
+import { RatesModule } from '../rates/rates.module'
+import { TransactionsModule } from '../transactions/transactions.module'
+import { User } from '../auth/entities/user.entity'
+import { WalletController } from './wallet.controller'
+import { WalletService } from './wallet.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), BlockchainModule, RatesModule],
+  imports:     [TypeOrmModule.forFeature([User]), BlockchainModule, RatesModule, TransactionsModule],
   controllers: [WalletController],
-  providers: [WalletService],
-  exports: [WalletService],
+  providers:   [WalletService],
+  exports:     [WalletService],
 })
 export class WalletModule {}
