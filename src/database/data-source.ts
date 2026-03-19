9bbc66d3cccbaf501c1e523b5a3ad4801024e99d
xaxxoo <bnahmad83@gmail.com>
2026-03-19 14:03:54 +0100

    migration sripts added

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   port:     parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USER     || 'postgres',
   password: process.env.DB_PASS     || 'postgres',
-  database: process.env.DB_NAME     || 'cheese_waitlist',
+  database: process.env.DB_NAME     || 'cheese_pay',
   entities:   ['src/**/*.entity.ts'],
   migrations: ['src/database/migrations/*.ts'],
   ssl:
