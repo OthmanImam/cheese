e600d98b800862b2fd5a5a1139a9abe9cd493daf
xaxxoo <bnahmad83@gmail.com>
2026-04-05 08:42:54 +0100

    auth module fixed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bullmq';
 import { OtpModule } from '../otp/otp.module';
 import { EmailModule } from '../email/email.module';
 import { WaitlistModule } from '../waitlist/waitlist.module';
 import { BlockchainModule } from '../blockchain/blockchain.module';
-import {WalletModule} from "../wallet/wallet.module";
+import { WalletModule } from '../wallet/wallet.module';
 import { Device } from '../devices/entities/device.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -17,29 +18,33 @@ import { JwtAccessStrategy } from './strategies/jwt-access.strategy';
 import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
 import { WaitlistEntry } from '../waitlist/entities/waitlist-entry.entity';
 import { ReferralEvent } from '../waitlist/entities/referral-event.entity';
-import { BullModule } from '@nestjs/bullmq';
 import { WalletCreationProcessor } from './processors/wallet-creation.processor';
-
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, 
-                              RefreshToken, 
-                              Device, 
-                              WaitlistEntry, 
-                              ReferralEvent,  
+    TypeOrmModule.forFeature([
+      User,
+      RefreshToken,
+      Device,
+      WaitlistEntry,
+      ReferralEvent,
+    ]),
+    BullModule.registerQueue({ name: 'wallet-creation' }),
     PassportModule,
-    JwtModule.register({}), // secrets supplied per sign() call
+    JwtModule.register({}),
     OtpModule,
     BlockchainModule,
     WalletModule,
     EmailModule,
     WaitlistModule,
-    ],
-  BullModule.registerQueue({ name: 'wallet-creation' })]),
-
+  ],
   controllers: [AuthController],
-  providers: [AuthService, JwtAccessStrategy, JwtRefreshStrategy, WalletCreationProcessor],
+  providers: [
+    AuthService,
+    JwtAccessStrategy,
+    JwtRefreshStrategy,
+    WalletCreationProcessor,
+  ],
   exports: [AuthService, TypeOrmModule, WaitlistModule],
 })
-export class AuthModule {}
+export class AuthModule { }
